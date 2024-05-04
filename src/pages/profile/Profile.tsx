82434b83b691b42ebf820f37da7cff987b6e5a97
Jefferson Capozzi <jefferson@ciandt.com>
2024-05-04 14:09:04 -0300

    Shadcn

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Profile() {
   return (
     <div>
       <h1>Profile</h1>
+      <Button>Button</Button>
     </div>
   );
 }
